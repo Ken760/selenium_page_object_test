3f7ffa62cdf530fbd9d2880bf249d941d1a49e27
Ken760 <49761163+Ken760@users.noreply.github.com>
2020-11-24 15:13:09 +0300

    Page Object style

--- conftest.py
+++ conftest.py
@@ -1,9 +1,12 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 
 def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default='ru',
                      help="Choose language: ec or fr")
 
@@ -13,7 +16,8 @@ def browser(request):
     user_language = request.config.getoption("language")
     print("\nstart chrome browser for test..")
     options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    options.add_experimental_option('prefs',
+                                    {'intl.accept_languages': user_language})
     browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
--- test_main_page.py
+++ test_main_page.py
@@ -1,29 +1,15 @@
 #для запуска теста написать команду
 #pytest --language=es test_main_page.py
 #language=(выбор языка)
-from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
 import time
-from selenium.webdriver.common.keys import Keys
-
-link = "http://selenium1py.pythonanywhere.com/"
-
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
-    go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
-
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
-    sign_in = driver.find_element(
-        By.XPATH, "//form[@class='authentication']//input[@type = 'text']"
-    )
-    sign_in.click()
-    sign_in.send_keys('mail' + Keys.TAB + 'password' + Keys.ENTER)
-    time.sleep(5)
 
 
 
