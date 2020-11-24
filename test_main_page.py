087737798925ed9841d00b875516fe2aa4dfef35
Ken760 <49761163+Ken760@users.noreply.github.com>
2020-11-24 12:28:54 +0300

    Initial commit

---
+++ test_main_page.py
@@ -0,0 +1,21 @@
+#для запуска теста написать команду
+#pytest --language=es test_main_page.py
+#language=(выбор языка)
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_check_add_to_basket_button(browser):
+    print("start test")
+    browser.get(link)
+    time.sleep(30)
+    browser.implicitly_wait(10)
+    try:
+        button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
+        print(f"button text = {button.text}")
+    except NoSuchElementException:
+        assert False, 'button has not find'
+    print("finish test1")
